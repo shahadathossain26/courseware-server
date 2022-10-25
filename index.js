e98b2b0479643c8baecb24af324e33591a0fb196
shahadathossain26 <shiimran172372@gmail.com>
2022-10-25 22:31:51 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express();
+const port = process.env.PORT || 5000;
+
+const courses = require('./data/courses.json')
+
+app.get('/', (req,res) =>{
+    res.send('course api running')
+})
+app.get('/courses', (req,res) =>{
+    res.send(courses)
+})
+
+app.listen(port, ()=>{
+    console.log('courseware server running on port', {port})
+})
