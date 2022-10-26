460b9864dd00434f3b66010f7bc146f91c7b0ad0
shahadathossain26 <shiimran172372@gmail.com>
2022-10-26 23:01:19 +0600

    courses dynamic api added

--- index.js
+++ index.js
@@ -14,6 +14,11 @@ app.get('/', (req, res) => {
 app.get('/courses', (req, res) => {
     res.send(courses)
 })
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = courses.find(course => course.id===id);
+    res.send(selectedCourse);
+})
 
 
 app.listen(port, () => {
