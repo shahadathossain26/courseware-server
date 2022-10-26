1b37af0ad2943d54a40ba7b1bbc23325ce2c2355
shahadathossain26 <shiimran172372@gmail.com>
2022-10-26 23:00:16 +0600

    courses api added

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const express = require('express')
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 
-const courses = require('./data/courses.json')
+app.use(cors());
 
-app.get('/', (req,res) =>{
+const courses = require('./data/courses.json');
+
+
+app.get('/', (req, res) => {
     res.send('course api running')
 })
-app.get('/courses', (req,res) =>{
+app.get('/courses', (req, res) => {
     res.send(courses)
 })
 
-app.listen(port, ()=>{
-    console.log('courseware server running on port', {port})
+
+app.listen(port, () => {
+    console.log('courseware server running on port', { port })
 })
